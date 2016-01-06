5a096ae6c2656b3e0478efd9167cc14379c06169
Martin Keršner <m.kersner@gmail.com>
2016-01-06 15:22:21 +0900

    changed preprocessing

--- util/preprocess_image.py
+++ util/preprocess_image.py
@@ -8,13 +8,14 @@ def preprocess_image(img, img_sz):
   # B 104.00698793 G 116.66876762 R 122.67891434
   mean = np.array([104.00698793, 116.66876762, 122.67891434])
   
-  I = img * 255.0
+  I = np.array(img, dtype=np.float32)
+  I = I[:,:,::-1]
   I_zero_mean = I - mean
 
   row_pad = img_sz - I_zero_mean.shape[0]
   col_pad = img_sz - I_zero_mean.shape[1]
+
   im = np.pad(I_zero_mean, ((0,row_pad), (0,col_pad), (0,0)), 'constant', constant_values=(0))
-  
-  im = im[:,:,:,np.newaxis]
+  im = im.transpose((2,0,1))
 
   return im
